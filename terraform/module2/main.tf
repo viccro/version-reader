4c3d4098948d99fc3582eeb99456a31ab2ca9f7c
viccro <vicki.crosson@gmail.com>
2023-04-27 13:36:22 -0400

    introduced m2

---
+++ terraform/module2/main.tf
@@ -0,0 +1,8 @@
+module "hello1" {
+  source = "git::https://github.com/viccro/version-source//module2?ref=v0.0.1-mod2"
+  name   = "vicki"
+}
+
+output "gh-output" {
+  value = module.hello1.hello_world
+}
