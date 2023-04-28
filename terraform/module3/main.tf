b04d2a5a0280750dda3daee379f4633d5a32803f
viccro <vicki.crosson@gmail.com>
2023-04-28 10:56:10 -0400

    removed m3

--- terraform/module3/main.tf
+++
@@ -1,8 +0,0 @@
-module "hello3-internal" {
-  source = "git::https://github.com/nytimes/viccro-test-version-src//module1?ref=v0.0.1-mod1"
-  name   = "vicki"
-}
-
-output "gh-output" {
-  value = module.hello3-internal.hello_world
-}
